61da19e8f4fc914dee916e4c8d31c65e6fd06ac3
Natorion <natorionj@gmail.com>
2025-06-24 13:16:30 -0500

    working on string stuff

---
+++ String/String_lib/readme.txt
@@ -0,0 +1,1 @@
+This diretory will go over writing your own string library since strings are written as  
---
+++ String/String_lib/stringlib.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t my_Strnlen(const char *s){
+	// walk s with a pointer until you hit '\0'
+
+
+}
+
+
+char *my_strcpy(char *dest, const char *src){
+	//copy bites including the trailing '\0' from src to dest
+
+
+} 
+
+
+int my_strcmp(const char *a, const char*b){
+	// compare *a vs *b byte by byte, returning <0,0,or>0
+
+
+
+} 
+
+void my_strrev(char *s){
+
+
+}
+
+
+int main(){
+
+	return 0; 
+
+}
